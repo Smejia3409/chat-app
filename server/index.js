07619d0ba2566ea3dbddc979c4dcbb84da8e2a25
Sandro Mejia <sandromejia3409@gmail.com>
2022-08-15 22:19:40 -0400

    added frontend react files

--- server/index.js
+++ server/index.js
@@ -4,10 +4,29 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 
+const { Server } = require("socket.io");
+
 //to handle cors issues
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5000/",
+    methods: ["GET, POST"],
+  },
+});
+
+//socket.io listen for events
+//connection is a event in socket
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  io.on("disconnect", () => {
+    console.log("User disconnected ", socket.id);
+  });
+});
 
 server.listen(5000, () => {
   console.log("server running on port 5000");
