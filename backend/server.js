82243cc1b0bb05e5802ca4543944d299390fb9e8
Sandro Mejia <sandromejia3409@gmail.com>
2022-08-22 23:09:32 -0400

    started users model for mongo backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-
 const http = require("http");
 const cors = require("cors");
-
 const { Server } = require("socket.io");
+const dotenv = require("dotenv").config();
 
 //to handle cors issues
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const server = http.createServer(app);
 
@@ -39,6 +41,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
   console.log("server running on port 5000");
 });
